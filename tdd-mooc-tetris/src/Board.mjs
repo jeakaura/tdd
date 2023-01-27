0d7d8bfb79a92f2aab638f5fd9992cf81142d6b0
Joona Kauranen <101410071+jeakaura@users.noreply.github.com>
2023-01-27 12:10:39 +0200

    Moves down one row per tick

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -2,6 +2,7 @@ export class Board {
   width;
   height;
   falling = false;
+  fallingBlockRow = 0;
 
   constructor(width, height) {
     this.width = width;
@@ -12,7 +13,7 @@ export class Board {
     let s = "";
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        if (this.falling && row == 0 && col == 1) {
+        if (this.falling && row == this.fallingBlockRow && col == 1) {
           s += this.color;
         } else {
           s += ".";
@@ -26,5 +27,9 @@ export class Board {
   drop(obj) {
     this.falling = true;
     this.color = obj.color;
+  }
+
+  tick() {
+    this.fallingBlockRow++;
   }
 }
